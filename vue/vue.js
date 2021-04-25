78593081dede4c8b0cb0ecfb5068eb5c31a23e6d
wushaohui <wushaohui@edusoho.com>
2021-04-25 17:20:10 +0800

    ice

--- vue/vue.js
+++ vue/vue.js
@@ -3,7 +3,7 @@
  * (c) 2014-2020 Evan You
  * Released under the MIT License.
  */
-(function (global, factory) {
+(function (global, factory) { 
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
   typeof define === 'function' && define.amd ? define(factory) :
   (global = global || self, global.Vue = factory());
